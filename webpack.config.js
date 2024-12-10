22d500381c41e22c8ec401499cdcedd2e792f84b
Mazur Oleksandr <dneruk@gmail.com>
2024-12-10 12:15:26 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,33 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: "production",
-  entry: "./app/index.ts",
+  mode: 'development',
+  entry: './app/index.ts',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 3000,
   },
   module: {
     rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg)$/,
-        type: "asset/resource",
-      },
-      {
-        test: /\.html$/,
-        use: "html-loader",
-      },
+      { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] },
+      { test: /\.(png|jpg|gif|svg)$/, use: 'file-loader' },
+      { test: /\.html$/, use: 'html-loader' },
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: "styles.css",
-    }),
-    new HtmlWebpackPlugin({
-      template: "./app/index.html",
-    }),
+    new HtmlWebpackPlugin({ template: './app/index.html' }),
+    new MiniCssExtractPlugin(),
+    new webpack.DefinePlugin({ ENV: JSON.stringify('development') }),
   ],
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
 };
