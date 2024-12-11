134ee5f4566d47b3b789cbcd55aee6600de164bb
Stepan Starodubtsev <stepan.starodubtsev@devops.ncia.nato.int>
2024-12-11 19:27:42 +0200

    Task done

--- webpack.config.js
+++ webpack.config.js
@@ -16,18 +16,52 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
-      { test: /\.scss$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] },
-      { test: /\.(png|jpg|gif|svg)$/, use: 'file-loader' },
-      { test: /\.html$/, use: 'html-loader' },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // Extract CSS into separate files
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: true,
+            },
+          },
+        ],
+      },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './app/index.html' }),
-    new MiniCssExtractPlugin(),
-    new webpack.DefinePlugin({ ENV: JSON.stringify('development') }),
-  ],
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new webpack.DefinePlugin({
+      ENV: JSON.stringify('dev'),
+    }),
+  ],
+  devtool: 'inline-source-map',
 };
