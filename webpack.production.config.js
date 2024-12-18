44d780dafadefdd86f4117a76feab17f31979638
vladossiks <vlad280404gavriluk@gmail.com>
2024-12-18 09:54:42 +0200

    Gavrik333

---
+++ webpack.production.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./app/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./app/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
